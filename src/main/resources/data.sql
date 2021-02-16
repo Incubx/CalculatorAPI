6eac742f1672ac4d43473541a06cca91b3d2d5b5
Incubx <denis-shevtsov-99@mail.ru>
2021-02-16 22:06:26 +0300

    H2 database was connected, Users table is created

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS billionaires;
+
+CREATE TABLE users (
+                              id INT AUTO_INCREMENT  PRIMARY KEY,
+                              username VARCHAR(250) NOT NULL,
+                              password VARCHAR(250) NOT NULL
+
+);

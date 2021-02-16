3185c3d0487e20b09eea39187808c436d6fa4de4
Incubx <denis-shevtsov-99@mail.ru>
2021-02-16 21:34:30 +0300

    evaluateExpression(String expression) controller's method was added

--- src/main/java/shevtsov/denis/calculator_api/Controller/CalculatorController.java
+++ src/main/java/shevtsov/denis/calculator_api/Controller/CalculatorController.java
@@ -1,8 +1,40 @@
 package shevtsov.denis.calculator_api.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import shevtsov.denis.calculator_api.Service.CalculatorService;
 
 @RestController
+@RequestMapping("evaluate")
 public class CalculatorController {
 
+
+    private CalculatorService calculatorService;
+
+    @Autowired
+    public void setCalculatorService(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+
+    @GetMapping("")
+    public ResponseEntity<Double> evaluateExpression(@RequestParam("expression") String expression){
+        System.out.println(expression);
+        try{
+            double result = calculatorService.evaluate(expression);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        }
+        catch (IllegalArgumentException ex){
+            return new ResponseEntity<>(null,HttpStatus.BAD_REQUEST);
+        }
+        catch (ArithmeticException ex){
+            return new ResponseEntity<>(null,HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+    }
 }
